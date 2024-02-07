dc4d0fc84d1dac282759e4b46d44905324ebe11e
Phuong Bui <buiphuongaptutc@gmail.com>
2024-02-07 16:58:56 +0700

    dynamic aws client config

--- app/page.tsx
+++ app/page.tsx
@@ -1,33 +1,67 @@
 'use client'
 
 import { upload } from "@/services/s3";
+import { message } from '@tauri-apps/api/dialog';
 import { useState } from "react";
-import { message, open } from '@tauri-apps/api/dialog';
 
 export default function Home() {
+  const [bucketName, setBucketName] = useState<string>('');
+  const [region, setRegion] = useState<string>('');
+  const [accessKeyId, setAccessKeyId] = useState<string>('');
+  const [secretAccessKey, setSecretAccessKey] = useState<string>('');
+  const [useLocalStack, setUseLocalStack] = useState<boolean>(true);
   const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
   const handleFileChange = (event: any) => {
     setSelectedFile(event.target.files[0]);
   };
 
+  const handleTextChange = (event: any, setState: (value: any) => any) => {
+    setState(event.target.value as string);
+  };
+
+  const handleCheckboxChange = (event: any, setState: (value: any) => any) => {
+    setState(event.target.checked as boolean);
+  };
+
   const submit = async () => {
     console.log(selectedFile);
     if (selectedFile) {
       try {
-        await upload(selectedFile)
+        await upload(selectedFile, {
+          bucketName,
+          useLocalStack,
+          region,
+          accessKeyId,
+          secretAccessKey
+        })
         await message('Uploaded file successfully!', { title: 'Success', type: 'info' })
       } catch (error) {
         console.error(error);
-        await message('Failed to upload file!', { title: 'Error', type: 'error' })
+        await message(`Failed to upload file!\n\n${error}`, { title: 'Error', type: 'error' })
       }
     }
   }
 
   return (
-    <main className="flex justify-center pt-10">
+    <main className="flex flex-col pt-5 px-10 gap-1">
+      <label htmlFor="" className="font-semibold">AWS Client Configuration</label>
+      <div className="flex gap-1 flex-wrap">
+        <input type="text" className="rounded border-2 py-1 px-3 flex-grow" placeholder="Bucket Name" value={bucketName} onChange={e => handleTextChange(e, setBucketName)} />
+        <input type="text" className="rounded border-2 py-1 px-3 flex-grow" placeholder="Region" value={region} onChange={e => handleTextChange(e, setRegion)} />
+      </div>
+      <div className="flex gap-1 flex-wrap">
+        <input type="text" className="rounded border-2 py-1 px-3 flex-grow" placeholder="Access Key ID" value={accessKeyId} onChange={e => handleTextChange(e, setAccessKeyId)} />
+        <input type="text" className="rounded border-2 py-1 px-3 flex-grow" placeholder="Secret Access Key" value={secretAccessKey} onChange={e => handleTextChange(e, setSecretAccessKey)} />
+      </div>
+      <div className="flex gap-1 flex-wrap">
+        <input type="checkbox" className="rounded border-2 py-1 px-3" placeholder="Secret Access Key" checked={useLocalStack} onChange={e => handleCheckboxChange(e, setUseLocalStack)} />
+        <label htmlFor="">Use LocalStack</label>
+      </div>
+      <hr className="my-3" />
+      <label htmlFor="" className="font-semibold">AWS S3 Upload</label>
       <input type="file" onChange={handleFileChange} />
-      <button onClick={submit} className="bg-green-300 text-green-900 px-3 rounded">Submit</button>
+      <button onClick={submit} className="bg-emerald-300 text-emerald-900 px-3 py-1 rounded">Upload</button>
     </main>
   );
 }
--- services/s3.ts
+++ services/s3.ts
@@ -1,25 +1,27 @@
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 
-const baseCfg = {
-    region: process.env.NEXT_PUBLIC_AWS_S3_REGION || 'us-east-1',
-    credentials: {
-        accessKeyId: process.env.NEXT_PUBLIC_AWS_S3_ACCESS_KEY_ID || 'test',
-        secretAccessKey: process.env.NEXT_PUBLIC_AWS_S3_SECRET_ACCESS_KEY || 'test',
-    },
-}
-const s3Mode = process.env.NEXT_PUBLIC_AWS_S3_MODE || 'local'
-const cfg = s3Mode !== 'local'
-    ? baseCfg
-    : {
-        ...baseCfg,
-        endpoint: 'http://s3.localhost.localstack.cloud:4566',
+async function upload(file: File, cfg: {
+    bucketName: string,
+    useLocalStack: boolean,
+    region: string,
+    accessKeyId: string,
+    secretAccessKey: string,
+}) {
+    const baseCfg = {
+        region: cfg.region,
+        credentials: {
+            accessKeyId: cfg.accessKeyId,
+            secretAccessKey: cfg.secretAccessKey,
+        },
     }
-
-const s3 = new S3Client(cfg);
-
-async function upload(file: File) {
+    const s3 = new S3Client(cfg.useLocalStack
+        ? {
+            ...baseCfg,
+            endpoint: 'http://s3.localhost.localstack.cloud:4566',
+        }
+        : baseCfg);
     const params = {
-        Bucket: process.env.NEXT_PUBLIC_AWS_S3_BUCKET_NAME || 'tauri-s3',
+        Bucket: cfg.bucketName,
         Key: file.name,
         Body: file
     };
